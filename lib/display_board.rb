3267dcd4ce5c3f4972c78ceb3025038a215ddfc1
Spencer Purcell <spurcell99@gmail.com>
2018-08-09 17:10:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  |   |
------------
-  |   |
------------
-  |   |
+cell = " | | "
+divider = '-----------'
+cell = " | | "
+divider = '-----------'
+cell = " | |"
+puts cell
+puts divider
+puts cell
+puts divider
 end
