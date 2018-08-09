79d48d87e91729a810f7910a402f23fc51a6284e
Spencer Purcell <spurcell99@gmail.com>
2018-08-09 17:25:04 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
 cell = "|  |"
-r
+row = "-----------"
+cell= "|   |"
+row = "-----------"
+puts cell
+puts row
+puts cell
+puts row
+end
